901e3f0ca9a439efe3955c49f4ede3461b13e4c7
Mehrdad Javan <mehrdad.javan@adapta.se>
2023-01-16 11:19:50 +0100

    axios post, put & delete

--- src/components/AxiosExample.js
+++ src/components/AxiosExample.js
@@ -34,6 +34,64 @@ const AxiosExample = () => {
 
     }
 
+    const sendPostRequest = async () => {
+        const data = {firstName: 'Test', lastName: 'test', email: 'test@test.se', title: 'TEST'};
+
+       await axios.post(API_URL, data)
+        .then(response => {
+            console.log("RESPONSE:", response);
+            if(response.status === 201){
+                setMessage({value: 'post operation is done!', type: 'success'});
+                sendGetRequest();
+            }
+
+        })
+        .catch(error => {
+            console.log("Error: ", error);
+            if(error){
+                setMessage({value: error.message, type: 'danger'});
+            }
+        });
+
+
+    }
+
+    const sendDeleteRequest = async () => {
+        const id = 8;
+
+        await axios.delete(API_URL+ '/' + id).then(response=> {
+            if(response.status === 204){
+                setMessage({value: 'delete operation is done!', type: 'success'});
+                sendGetRequest();
+            }
+
+        }).catch(error=> {
+            console.log("Error: ", error);
+            if(error){
+                setMessage({value: error.message, type: 'danger'});
+            }
+        });
+
+    }
+
+    const sendPutequest = async () => {
+        const data = {id: 7, firstName: 'Simon', lastName: 'Elbrink', email: 'test@test.se', title: 'TEST'};
+
+        await axios.put(API_URL, data)
+        .then(response => {
+            if(response.status === 204){
+                setMessage({value: 'update operation is done!', type: 'success'});
+                sendGetRequest();
+            }
+
+        }).catch( error => {
+            console.log("Error: ", error);
+            if(error){
+                setMessage({value: error.message, type: 'danger'});
+            }
+        });
+
+    }
 
     return (
         <div className='container'>
@@ -42,7 +100,7 @@ const AxiosExample = () => {
 
             <div className='card'>
                 <div className='card-header bg-dark text-white'>
-                    Example!: Call Get API
+                    Example1: Call Get API
                 </div>
                 <div className='card-body'>
                     <button type='button' className='btn btn-info' onClick={sendGetRequest} >Get List</button>
@@ -53,6 +111,48 @@ const AxiosExample = () => {
                 <div className='card-footer'></div>
             </div>
             
+            <br/>
+
+            <div className='card'>
+                <div className='card-header bg-dark text-white'>
+                    Example2: Call Post API
+                </div>
+                <div className='card-body'>
+                    <button type='button' className='btn btn-success' onClick={sendPostRequest} >Register Person</button>
+                </div>
+                <br/>
+
+                <div className='card-footer'></div>
+            </div>
+
+            <br/>
+
+            <div className='card'>
+                <div className='card-header bg-dark text-white'>
+                    Example3: Call Delete API
+                </div>
+                <div className='card-body'>
+                    <button type='button' className='btn btn-danger' onClick={sendDeleteRequest} >Delete Person</button>
+                </div>
+                <br/>
+
+                <div className='card-footer'></div>
+            </div>
+
+
+            <br/>
+
+            <div className='card'>
+                <div className='card-header bg-dark text-white'>
+                    Example4: Call Put API
+                </div>
+                <div className='card-body'>
+                    <button type='button' className='btn btn-warning' onClick={sendPutequest} >Update Person</button>
+                </div>
+                <br/>
+
+                <div className='card-footer'></div>
+            </div>
         </div>
     );
 
